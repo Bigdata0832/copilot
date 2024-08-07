74bdc634e9ee1c36e9bd739d51e09715a3673dc4
Lucien Lin <lucien.lin@big-data.com.tw>
2024-08-07 16:13:56 +0800

    Adjust streamlit and delete deprecated api file

--- streamlit.py
+++ streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from utils.llm.dep_assistant import create_thread, delete_thread, create_messages_to_thread, run_assistant, get_chat_history
+from datetime import datetime
+from src.logic import start_chat, clear_chat, select_assistant, send_message
 
 def main():
     st.title("KEYPO Copilot")
@@ -14,43 +15,42 @@ def main():
     with st.sidebar:
         st.header("Thread Dashboard")
         if st.button("Create"):
-            thread = create_thread()
-            st.session_state.thread_id = thread.id
+            thread_id = start_chat()
+            st.session_state.thread_id = thread_id
             st.write(f"New thread created with ID: {st.session_state.thread_id}")
         if st.button("Clear"):
             if st.session_state.thread_id:
-                delete_thread(st.session_state.thread_id)
+                clear_chat(st.session_state.thread_id)
                 st.write(f"The thread with ID {st.session_state.thread_id} has been cleared!")
                 st.session_state.thread_id = None
                 st.session_state.assistant_id = None
 
     # 右側邊欄 - Copilot Version
     with st.expander("Copilot Version"):
-        if st.button("輿情1.0.1"):
-            st.session_state.assistant_id = "asst_Q0aogQUlR5PkLuBB6Hmh6mk5"
-            st.write("Using Version 1.0.1")
+        col1, col2, _ = st.columns([1, 1, 7])
+        with col1:
+            if st.button("1.0.1"):
+                st.session_state.assistant_id = select_assistant(name="KEYPO Copilot")
+                st.write("Using Version 1.0.1")
+        with col2:
+            if st.button("1.0.2"):
+                st.session_state.assistant_id = select_assistant(name="KEYPO Copilot2.0")
+                st.write("Using Version 1.0.2")
 
     # 主功能區域
     if st.session_state.thread_id and st.session_state.assistant_id:
         prompt = st.text_input("User:")
         if st.button("Submit"):
-            create_messages_to_thread(
+            chat_history = send_message(
+                user_message=prompt,
+                assistant_id=st.session_state.assistant_id,
                 thread_id=st.session_state.thread_id,
-                role="user",
-                content=prompt
             )
-            run = run_assistant(thread_id=st.session_state.thread_id, assistant_id=st.session_state.assistant_id)
-            chat_history = get_chat_history(run=run, thread_id=st.session_state.thread_id)
-            st.write(chat_history[-1])
-
-        if st.button("Chat History Submit"):
-            create_messages_to_thread(
-                thread_id=st.session_state.thread_id,
-                role="user",
-                content=prompt
-            )
-            run = run_assistant(thread_id=st.session_state.thread_id, assistant_id=st.session_state.assistant_id)
-            chat_history = get_chat_history(run=run, thread_id=st.session_state.thread_id)
+            resp = chat_history[-1].replace("assistant: ","")
+            st.write(f"KEYPO Copilot: {resp}")
+            st.divider()
+            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            st.write(f"Response received at: {current_time}")
             st.write(chat_history)
 
 if __name__ == "__main__":
